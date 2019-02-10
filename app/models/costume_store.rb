159f9adf577ea7f013f9189d88d82d73df22eaa9
tatiana.mojica <tatiana.mojica@bustle.com>
2019-02-10 04:47:28 +0000

    Automatically backed up by Learn

--- app/models/costume_store.rb
+++ app/models/costume_store.rb
@@ -1,5 +1,5 @@
 # Create your CostumeStore class here
-class CreateCostumeStores < ActiveRecord::Migration
+class CreateCostumeStores < ActiveRecord::Migration[4.2]
   def change
     create_table :costume_stores do |t|
       t.text :name
