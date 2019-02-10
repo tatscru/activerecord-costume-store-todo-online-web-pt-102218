fb9f93f56e3acf67942d7c54bfabd45b8929fe1a
tatiana.mojica <tatiana.mojica@bustle.com>
2019-02-10 04:45:08 +0000

    Automatically backed up by Learn

--- app/models/costume_store.rb
+++ app/models/costume_store.rb
@@ -1,1 +1,13 @@
 # Create your CostumeStore class here
+
+class CreateCostumes < ActiveRecord::Migration[4.2]
+  def change 
+    create_table :costumes do |t|
+      t.text :name 
+      t.float :price
+      t.integer :size 
+      t.text :imageurl 
+      t.timestamps 
+    end 
+  end 
+end 
