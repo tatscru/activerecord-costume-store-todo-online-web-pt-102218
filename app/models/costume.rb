a7f7e6e4e479fb266cd7a3f4abb61fbd9bf71a37
tatiana.mojica <tatiana.mojica@bustle.com>
2019-02-10 04:50:09 +0000

    Automatically backed up by Learn

--- app/models/costume.rb
+++ app/models/costume.rb
@@ -2,7 +2,7 @@
 # It should inherit from ActiveRecord::Base
 class CreateCostumes < ActiveRecord::Migration[4.2]
   def change 
-    create_table :costume do |t|
+    create_table :costumes do |t|
       t.text :name 
       t.float :price
       t.integer :size 
