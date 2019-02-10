2c0215a6b7019a6ff5e893df79d9d1cb9e25540c
tatiana.mojica <tatiana.mojica@bustle.com>
2019-02-10 04:59:50 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,1 +1,16 @@
 # Create your haunted_houses migration here
+
+class CreateHauntedHouses < ActiveRecord::Migration
+  def change
+    create_table :haunted_houses do |t|
+      t.string :name
+      t.string :location
+      t.string :theme
+      t.boolean :family_friendly
+      t.date :opening_date
+      t.date :closing_date
+      t.string :long_description
+      t.timestamps
+    end
+  end
+end
