f0efeeecbfcb24f0087f54f30d0cca87d2d181fc
tatiana.mojica <tatiana.mojica@bustle.com>
2019-02-10 05:08:54 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -15,7 +15,7 @@
 # end 
 
 
-class CreateCostumeStores < ActiveRecord::Migration
+class CreateCostumeStores < ActiveRecord::Migration[5.0]
   def change
     create_table :costume_stores do |t|
       t.text :name
