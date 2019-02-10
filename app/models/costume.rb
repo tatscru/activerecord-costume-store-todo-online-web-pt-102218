7e34d505c845dc5f67623056dd843b19db7d97cf
tatiana.mojica <tatiana.mojica@bustle.com>
2019-02-10 04:39:15 +0000

    Automatically backed up by Learn

--- app/models/costume.rb
+++ app/models/costume.rb
@@ -6,15 +6,6 @@ class CreateCostume < ActiveRecord::Migration
       t.text :name 
       t.float :price
       t.integer :size 
-      t.string :imageurl 
+      t.text :imageurl 
   end 
 end 
-
-class CreateCostumes < ActiveRecord::Migration
-  def change
-    create_table :costumes do |t|
-      t.text :name
-      t.float :price
-      t.integer :size
-      t.text :image_url
-      t.timestamps
