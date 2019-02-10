21e5a557fc954f57444c5551bc9622ef98d998b5
tatiana.mojica <tatiana.mojica@bustle.com>
2019-02-10 05:12:56 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -3,8 +3,8 @@
 class CreateCostumeStores < ActiveRecord::Migration[5.0]
   def change
     create_table :costume_stores do |t|
-      t.text :name
-      t.text :location
+      t.string :name
+      t.string :location
       t.integer :costume_inventory
       t.integer :num_of_employees
       t.boolean :still_in_business
