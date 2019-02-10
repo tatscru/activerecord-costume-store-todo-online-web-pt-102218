01e6fbb83e6682e47ef32bfc2a1797007b4879a6
tatiana.mojica <tatiana.mojica@bustle.com>
2019-02-10 05:08:31 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,6 +1,21 @@
 # Create your costume_stores migration here
 
-class CreateCostumeStores < ActiveRecord::Migration[5.0]
+# class CreateCostumeStores < ActiveRecord::Migration[5.0]
+#   def change
+#     create_table :costume_stores do |t|
+#       t.text :name
+#       t.text :location
+#       t.integer :costume_inventory
+#       t.integer :num_of_employees
+#       t.boolean :still_in_business
+#       t.datetime :opening_time
+#       t.datetime :closing_time
+#     end 
+#   end 
+# end 
+
+
+class CreateCostumeStores < ActiveRecord::Migration
   def change
     create_table :costume_stores do |t|
       t.text :name
@@ -10,6 +25,7 @@ class CreateCostumeStores < ActiveRecord::Migration[5.0]
       t.boolean :still_in_business
       t.datetime :opening_time
       t.datetime :closing_time
-    end 
-  end 
-end 
+      t.timestamps
+    end
+  end
+end
